982be2a15724fa5bddeacb1c95053ce0f5ea71e6
Jester Triboulet <vitoralmeida2430@gmail.com>
2024-03-24 19:43:20 -0300

    update

---
+++ main.js
@@ -0,0 +1,14 @@
+console.log('Hello World!');
+
+function abrirNovaAba() {
+  window.open('https://www.instagram.com/p/C17Orf6LBoW/?igsh=bW5zM3I5Z2UxeWtx', '_blank');
+}
+
+function abrirPrologo() {
+  window.open('https://www.wattpad.com/1431820478?utm_source=android&utm_medium=link&utm_content=share_published&wp_page=create_on_publish&wp_uname=TribouletHellfire', '_blank');
+}
+
+function abrirBlog() {
+  window.open('https://jestertriboulet.blogspot.com/2023/09/como-vender-barato-atrapalha-outros.html?m=1', '_blank');
+}
+
